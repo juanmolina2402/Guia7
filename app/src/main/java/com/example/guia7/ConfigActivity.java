04ff740486a88171b4af2a8fd5aa95e7ca70ce63
juanmolina2402 <1cajuanmolina@gmail.com>
2021-10-13 20:03:17 -0500

    .

---
+++ app/src/main/java/com/example/guia7/ConfigActivity.java
@@ -0,0 +1,69 @@
+package com.example.guia7;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.io.File;
+
+public class ConfigActivity extends AppCompatActivity {
+    private EditText edtUser;
+    private Button btnSave;
+    public static String FILE_CONF = "configuration";
+    public SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        edtUser = findViewById(R.id.edtUser);
+        btnSave = findViewById(R.id.btnSave);
+        addToEditText();
+
+        btnSave.setOnClickListener( aux -> {
+            addUser();
+        });
+    }
+
+    private void addToEditText(){
+        sharedPreferences = getSharedPreferences(FILE_CONF, MODE_PRIVATE);
+        if(sharedPreferences != null){
+            String x = sharedPreferences.getString("USER", "");
+            edtUser.setText(x);
+        }
+    }
+
+    private void addUser() {
+        if(!edtUser.getText().toString().isEmpty()) {
+            sharedPreferences = getSharedPreferences(FILE_CONF, MODE_PRIVATE);
+            SharedPreferences.Editor editorConfig = sharedPreferences.edit();
+            editorConfig.putString("USER", edtUser.getText().toString());
+            editorConfig.commit();
+            Toast.makeText(ConfigActivity.this, "El usuario se guardó con éxito", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(ConfigActivity.this, MainActivity.class));
+            finish();
+        }else{
+            Toast.makeText(ConfigActivity.this, "Por favor, llene todos los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
+    }
+}
--- app/src/main/java/com/example/guia7/MainActivity.java
+++ app/src/main/java/com/example/guia7/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.guia7;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imgPlay = findViewById(R.id.imvPlay);
+        ImageView imgScore = findViewById(R.id.imvScore);
+        ImageView imgHelp = findViewById(R.id.imvHelp);
+        ImageView imgConfig = findViewById(R.id.imvConfig);
+        ImageView imgDatos = findViewById(R.id.imvDatos);
+
+        imgPlay.setOnClickListener(aux -> {
+                startActivity(new Intent(MainActivity.this, PlayActivity.class));
+        });
+
+        imgConfig.setOnClickListener(aux -> {
+            startActivity(new Intent(MainActivity.this, ConfigActivity.class));
+        });
+
+        imgDatos.setOnClickListener(aux -> {
+            startActivity(new Intent(MainActivity.this, DatosActivity.class));
+        });
     }
 }
